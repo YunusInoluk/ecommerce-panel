f040ef3f0b35b0f3d214a72f3a079cef1d0fe3c3
Yunus İnoluk <yunusinoluk@gmail.com>
2023-09-18 18:10:57 +0300

    modal infrastructure added

---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Modal from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+export const StoreModal = () => {
+  const storeModal = useStoreModal();
+  return (
+    <Modal
+      title="Create Store"
+      description="Add a new store"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      Create Store Form
+    </Modal>
+  );
+};
