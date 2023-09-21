cc410f214f4bab80710cd229fcc613c7e6f4d85e
Yunus İnoluk <yunusinoluk@gmail.com>
2023-09-21 14:51:37 +0300

    store edit delete added

---
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -0,0 +1,31 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import React from "react";
+import SettingsForm from "./components/settings-form";
+
+interface SettingsPageProps {
+  params: { storeId: string };
+}
+const SettingsPage: React.FC<SettingsPageProps> = async ({ params }) => {
+  const { userId } = auth();
+  if (!userId) return redirect("/sign-in");
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+  if (!store) return redirect("/");
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 p-8 pt-6 space-y-4">
+        <SettingsForm initialData={store} />
+      </div>
+    </div>
+  );
+};
+
+export default SettingsPage;
